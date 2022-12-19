34c8f6330232ebca154b391966287a2d20f5014c
tigistzelalem <makgg015@gmail.com>
2022-12-19 09:46:31 +0300

    insertionsort one

---
+++ Insertionsort_one/insertionsort_one.py
@@ -0,0 +1,13 @@
+def insertionSort1(n, arr):
+    # Write your code here
+    key = arr[-1]
+    i = n-1
+    while i > 0 and arr[i-1] > key:
+        arr[i] = arr[i-1]
+        print(*arr)
+        i-=1
+    arr[i] = key
+    print(*arr)
+    
+    
+    
