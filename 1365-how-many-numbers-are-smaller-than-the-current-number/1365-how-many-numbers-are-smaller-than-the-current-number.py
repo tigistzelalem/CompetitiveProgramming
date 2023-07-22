a14a1ef5545bf98e12b0bf1203fa01622cf14f64
Tigist Zelalem <91589299+tigistzelalem@users.noreply.github.com>
2023-07-22 10:52:20 +0300

    Time: 452 ms (14.74%), Space: 16.3 MB (71.01%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -3,9 +3,9 @@ class Solution:
         arr = []
         for i in range(len(nums)):
             count = 0
+            
             for j in range(len(nums)):
                 if j != i and nums[j] < nums[i]:
                     count += 1
             arr.append(count)
-                    
         return arr
