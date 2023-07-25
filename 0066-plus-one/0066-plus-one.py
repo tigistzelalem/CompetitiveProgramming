165894d290ce4c866899db51e71cc33854c45e76
Tigist Zelalem <91589299+tigistzelalem@users.noreply.github.com>
2023-07-25 11:39:06 +0300

    Time: 48 ms (61.45%), Space: 16.4 MB (32.95%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,12 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        
+        string = ""
+        for digit in digits:
+            string += str(digit)
+        number = int(string) + 1
+        
+        ans = []
+        for i in str(number):
+            ans.append(int(i))
+        return ans
