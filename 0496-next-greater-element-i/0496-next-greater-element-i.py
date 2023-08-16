92bb24e0e09bd7dd11c72b5e2c9979a0ade976b0
Tigist Zelalem <91589299+tigistzelalem@users.noreply.github.com>
2023-08-16 20:03:14 +0300

    Time: 48 ms (94.72%), Space: 16.7 MB (37.04%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,17 +1,28 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        n1 = len(nums1)
+        n2 = len(nums2)
         stack = []
-        for i in nums1:
-            ans = -1
-            index = nums2.index(i)
-            temp = nums2[index+1:]
-            for j in temp:
-                if j > i:
-                    ans = j
-                    break
-            stack.append(ans)
-        return stack
-                
+        dict = {}
+        
+        for i in range(n2):
+            if not stack:
+                stack.append(nums2[i])
+            else:
+                while stack and stack[-1] < nums2[i]:
+                    pop = stack.pop()
+                    dict[pop] = nums2[i]
+                    
+                stack.append(nums2[i])
+        answer = []
+        for i in range(n1):
+            if nums1[i] not in dict:
+                dict[nums1[i]] = -1
+            answer.append(dict[nums1[i]])
+            
+    
+            
+        return answer
                         
             
 
