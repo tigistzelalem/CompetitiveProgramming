edab0476b7c7392078842e412c74add4efeb1495
Tigist Zelalem <91589299+tigistzelalem@users.noreply.github.com>
2023-10-04 15:05:07 +0300

    Time: 152 ms (51.36%), Space: 17.9 MB (16.32%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,12 +2,14 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         
         counts = {}
-        for _, val in enumerate(nums):
+        for i, val in enumerate(nums):
             counts[val] = counts.get(val, 0) + 1
-        threshold = len(nums) / 2
+            
+        freq = len(nums) / 2
         result = None
+        
         for num, count in counts.items():
-            if count > threshold:
+            if count > freq:
                 result = num
                 break   
         return num
