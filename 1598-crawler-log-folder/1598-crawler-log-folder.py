2675be53790b501f8101f21e9359a70424f7e441
Tigist Zelalem <91589299+tigistzelalem@users.noreply.github.com>
2023-10-06 10:20:39 +0300

    Time: 58 ms (30.52%), Space: 16.4 MB (90.61%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.py
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -1,14 +1,17 @@
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
         count = 0
+         
         for i in range(len(logs)):
-            if logs[i] == "../":
+            if logs[i] == '../':
                 if count:
                     count -= 1
-            elif logs[i] == "./":
-                continue
+            elif logs[i] == './':
+                    continue
             else:
-                count += 1
+                    count += 1
+        
         return count
             
-        
+
+            
