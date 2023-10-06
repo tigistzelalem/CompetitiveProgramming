59e1a932bbd7eae956d29972d29167c41595bc71
Tigist Zelalem <91589299+tigistzelalem@users.noreply.github.com>
2023-10-06 09:24:13 +0300

    Time: 48 ms (57.46%), Space: 16.3 MB (85.09%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -1,15 +1,20 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
+        
         stack = []
+        
         for i in operations:
             if any(map(str.isdigit, i)):
                 stack.append(int(i))
             if i == "+":
                 stack.append(stack[-1] + stack[-2])
-            elif i == "D":
-                stack.append(2*stack[-1])
             elif i == "C":
                 stack.pop()
+            elif i == "D":
+                stack.append(2*stack[-1])
         return sum(stack)
+        
+        
+
 
         
