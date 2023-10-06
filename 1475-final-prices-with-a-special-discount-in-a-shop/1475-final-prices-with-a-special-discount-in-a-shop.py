de2de0f9aab72d2b652f4b70ca7bf058847710ee
Tigist Zelalem <91589299+tigistzelalem@users.noreply.github.com>
2023-10-06 10:01:52 +0300

    Time: 94 ms (5.28%), Space: 16.4 MB (31.35%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,15 +1,16 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        
+        n = len(prices)
         stack = []
         
-        for i in range(len(prices)):
-            for j in range(1, len(prices)):
+        for i in range(n):
+            for j in range(1, n):
                 if j > i and prices[j] <= prices[i]:
                     stack.append(prices[i] - prices[j])
                     break
             else:
                 stack.append(prices[i])
+        
         return stack
     
  
