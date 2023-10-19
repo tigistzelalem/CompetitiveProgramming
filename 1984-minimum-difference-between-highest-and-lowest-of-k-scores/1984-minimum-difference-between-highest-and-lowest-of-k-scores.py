766cc8d43f176fd045efe7505937ae9bec1c739d
Tigist Zelalem <91589299+tigistzelalem@users.noreply.github.com>
2023-10-19 10:44:50 +0300

    Time: 106 ms (20.30%), Space: 16.5 MB (60.40%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -5,13 +5,14 @@ class Solution:
         nums.sort()
         min_diff = float('inf')
         
-
         for end in range(k-1, n):
             start = end - (k-1)
-            cur_diff = nums[end] - nums[start]
-            min_diff = min(min_diff, cur_diff)
+            curr_diff = nums[end] - nums[start]
+            min_diff = min(min_diff, curr_diff)
             
         return min_diff
+
+
             
             
         
