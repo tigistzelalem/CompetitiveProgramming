43d3d66509bad172be15e7dc268952f921b5a09c
Tigist Zelalem <91589299+tigistzelalem@users.noreply.github.com>
2023-10-19 09:54:14 +0300

    Time: 286 ms (70.75%), Space: 16.2 MB (87.18%) - LeetHub

--- 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.py
+++ 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.py
@@ -4,16 +4,16 @@ class Solution:
         longest = 0 
         current = 0 
         
-        for i in range(len(nums)): 
-            if nums[i] > threshold: 
-                current = 0 
-            elif current == 0 and nums[i] % 2 == 0: 
-                current = 1 
-            elif current > 0 and nums[i] % 2 != nums[i - 1] % 2: 
-                current += 1 
-            else: 
-                current = 1 if nums[i] % 2 == 0 else 0 
-                
-            longest = max(longest, current) 
+        for i in range(len(nums)):
+            if nums[i] > threshold:
+                current = 0
+            elif current == 0 and nums[i] % 2 == 0:
+                current = 1
+            elif current > 0 and nums[i] % 2 != nums[i-1] %2:
+                current += 1
+            else: current = 1 if nums[i] % 2 == 0 else 0
             
-        return longest 
+            longest = max(longest, current)
+        
+        return longest
+        
