cf9eb3a87f0a0534bde5c194148414b360374331
Tigist Zelalem <91589299+tigistzelalem@users.noreply.github.com>
2024-02-05 10:24:26 +0300

    Add solution for Sort Colors

--- leetcodesolution/leetcode/sort-colors.py
+++ leetcodesolution/leetcode/sort-colors.py
@@ -3,11 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)):
-            min_index = i
-            for j in  range(i+1, len(nums)):
-                if nums[j] < nums[min_index]:
-                    min_index = j
-            nums[i], nums[min_index] = nums[min_index], nums[i]
+
+        for left in range(len(nums)):
+            min_index = left
+            for right in range(left + 1, len(nums)):
+                if nums[right] < nums[min_index]:
+                    min_index = right
+            nums[left], nums[min_index] = nums[min_index], nums[left]
 
         
