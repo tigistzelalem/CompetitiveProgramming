7a740886c13b957bb526dc271e74d372105c5e1e
Tigist Zelalem <91589299+tigistzelalem@users.noreply.github.com>
2024-02-22 18:14:01 +0300

    Add solution for Remove Duplicate Letters

---
+++ leetcodesolution/leetcode/remove-duplicate-letters.py
@@ -0,0 +1,20 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        stack = []
+        count = Counter(s)
+        seen = set()
+        for char in s:
+            if char in seen:
+                count[char] -= 1
+                continue
+            while stack and stack[-1] > char and count[stack[-1]] > 0:
+                popped = stack.pop()
+                seen.remove(popped)
+
+            stack.append(char)
+            seen.add(char)
+            count[char] -= 1
+        
+        return ''.join(stack)
+
+        
